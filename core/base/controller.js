356ad8cc646885b47b2531d9071b6b45e2033924
Амаль <50665935+WTTeneger@users.noreply.github.com>
2023-02-18 12:14:12 +0700

    rs

--- core/base/controller.js
+++ core/base/controller.js
@@ -10,23 +10,37 @@ export async function getAlertData(req, res) {
     });
     console.log("ID", ID)
     console.log(req.body);
-    let { exchange, coin, direction, price } = req.body;
+    let { exchange, coin, direction, price, riskM } = req.body;
+
 
     // Проверить, что все поля есть
     if (!coin || !exchange || !direction || !price) return res.status(400).json({
         status: 'error',
         message: 'Not all fields are defined',
     });
+    // риск 7% по умолчанию
+    if (!riskM) riskM = 7;
 
     let binance = new BinanceAPI('lmTRpBu7E8AtRv5V79nesSKENGsRzjQ7PxzoHVEPd6rbwDTHWJXx5qrTHPzDxer2', '5sNtsrO3Gvt4vkxvDLue4GRtK0R2fVN9RMSDLidQ4pwYzdMwq4XU3SHglu7QuBRh');
     // купить 7% от баланса
-    // gjkex
     // let res = await binance.createOrder(coin, direction, 0.001, price, 'LIMIT', 'GTC', false, false, 'CONTRACT_PRICE', false)
 
     let balance = await binance.getBalance();
+    // получить баланс USDT
+    let usdt_balance = balance.filter(item => item.asset === 'USDT')[0];
+
+    let total_for_trade = usdt_balance.availableBalance * (riskM / 100);
+
+    // открыть сделку на покупку 7% от баланса
+    // var order = await binance.createOrder(coin, direction, total_for_trade / price, price, 'LIMIT', 'GTC', false, false, 'CONTRACT_PRICE', false)
 
     return res.json({
+        // order,
         status: 'ok',
-        balance,
+        total_for_trade,
+        usdt_balance,
+        // balance,
     })
+
+
 }
--- modules/binanceAPI.js
+++ modules/binanceAPI.js
@@ -1,50 +1,74 @@
 // API BINANCE CLASS через fatch
-
+import crypto from 'crypto';
+import fetch from 'node-fetch';
+import querystring from 'querystring';
 class BinanceAPI {
-    url = 'https://fapi.binance.com';
-    constructor(apiKey, apiSecret) { 
-        this.apiKey = apiKey;
-        this.apiSecret = apiSecret;
+    constructor(API_KEY, API_SECRET, futures = true, test_net=false) {
+        this.API_KEY = test_net ? 'vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A' : API_KEY;
+        this.API_SECRET = test_net ? 'NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j' : API_SECRET;
+        this.futures = futures;
+        this.base_url = test_net ? 'https://testnet.binancefuture.com' : this.futures ? 'https://fapi.binance.com/' : 'https://api.binance.com/'  ;
+        this.shift_seconds = 0;
+        this.methods = {
+            getFuturesBalance: {
+                url: '/fapi/v2/balance',
+                method: 'GET',
+                private: true
+            },
+            createFuturesOrder: {
+                url: '/fapi/v1/order',
+                method: 'POST',
+                private: true
+            },
+            // define your methods here
+        };
+
+        // console.log(this.base_url);
     }
 
-    // запросы к API
-    async _request(method, path, params) {
-        const url = this.url + path;
-        const timestamp = Date.now();
-        const query = Object.keys(params)
-            .map(key => `${key}=${encodeURIComponent(params[key])}`)
-            .join('&');
-        const signature = crypto
-            .createHmac('sha256', this.apiSecret)
-            .update(query + timestamp)
-            .digest('hex');
-        const headers = { 
-            'X-MBX-APIKEY': this.apiKey,
-            'Content-Type': 'application/x-www-form-urlencoded',
+    async request(command, kwargs = {}) {
+        const method = this.methods[command].method;
+        let payloadStr = querystring.stringify(kwargs);
+
+        if (this.methods[command].private) {
+            const timestamp = Date.now();
+            kwargs.timestamp = timestamp - this.shift_seconds - 1;
+            payloadStr = querystring.stringify(kwargs);
+            const signature = crypto.createHmac('sha256', this.API_SECRET).update(payloadStr).digest('hex');
+            payloadStr += `&signature=${signature}`;
+
+            console.log('payloadStr', payloadStr);
+        }
+
+        const url = this.base_url + this.methods[command].url;
+        const headers = {
+            'X-MBX-APIKEY': this.API_KEY
         };
+
         const options = {
             method,
             headers,
+            body: method === 'GET' ? undefined : payloadStr
         };
-        if (method === 'GET') {
-            const urlWithParams = `${url}?${query}&timestamp=${timestamp}&signature=${signature}`;
-            const response = await fetch(urlWithParams, options);
-            const result = await response.json();
-            return result;
+        
+        const response = await fetch(method === 'GET' ? url + '?' + payloadStr : url, options);
+
+        if (!response.ok) {
+            const errorResponse = await response.text();
+            throw new Error(`Binance API error: ${errorResponse}`);
         }
-        if (method === 'POST') {
-            const urlWithParams = `${url}?timestamp=${timestamp}&signature=${signature}`;
-            const response = await fetch(urlWithParams, {
-                ...options,
-                body: query,
-            });
-            const result = await response.json();
-            return result;
+
+        const data = await response.json();
+        console.log(data);
+        if (data.code) {
+            throw new Error(`Binance API error: ${data.msg}`);
         }
+
+        return data;
     }
 
     // создание сделки на покупку futures с 3 плечами
-    async createOrder(symbol, side, quantity, price, type, timeInForce, reduceOnly, closePosition, workingType, priceProtect) { 
+    async createOrder(symbol, side, quantity, price, type, timeInForce, reduceOnly, closePosition, workingType, priceProtect) {
         /*
         symbol: 'BTCUSDT', // это пара 
         side: 'BUY', // это направление
@@ -57,14 +81,16 @@ class BinanceAPI {
         workingType: 'CONTRACT_PRICE', // это 
         priceProtect: false // это не работает
         */
-        
+
+        // symbol=BTCUSDT&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559
+        // symbol=HOOKUSDT&side=buy&type=LIMIT&timeInForce=GTC&quantity=0.5253205963254592&price=2.6670000000000003&timestamp=1676660358124&recvWindow=5000
         const params = {
             symbol,
             side,
-            quantity,
-            price,
             type,
             timeInForce,
+            quantity,
+            price,
             reduceOnly,
             closePosition,
             workingType,
@@ -77,7 +103,7 @@ class BinanceAPI {
 
     // получить баланс на фючерсах
     async getBalance() {
-        const result = await this._request('GET', '/fapi/v2/balance', {});
+        const result = await this.request('getFuturesBalance', {});
         return result;
     }
 }
